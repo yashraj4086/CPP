950ad2f3dfb2e55ef07ba43a70914872faeac324
Yash Raj Times Tech <yashai4086@gmail.com>
2025-03-06 12:43:22 +0500

    Add files via upload
    A collection of string manipulation exercises with answers, covering common operations like concatenation, slicing, formatting, and pattern matching. Perfect for beginners and those looking to sharpen their CPP string skills. 🚀

---
+++ 24.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ using namespace std;
+ int main (){
+	
+/*Write a program that prints a string containing a double quote ("),
+	 like He said, "Hello!".*/
+	string doulbe = "He said, \"Hello!\"";
+	cout <<doulbe<<endl;
+	
+	
+	}
---
+++ 26.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ using namespace std;
+ int main (){
+	
+	
+
+
+	/*Write a program that demonstrates how to use escape sequences like \\, \', and \".
+	What will the following code print?
+	string txt = "He said, \"Hello World!\"";
+	cout << txt;*/
+	string text = "He said, \"Hello World!\"";
+	cout << ""<<text<<endl;
+}
