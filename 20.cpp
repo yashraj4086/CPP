02327ad5087d9c979b1479d0c48cba220cc3625e
Yash Raj Times Tech <yashai4086@gmail.com>
2025-03-06 12:41:32 +0500

    Add files via upload
    A collection of string manipulation exercises with answers, covering common operations like concatenation, slicing, formatting, and pattern matching. Perfect for beginners and those looking to sharpen their CPP string skills. 🚀

---
+++ 20.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main (){
+	
+
+
+	/*What will the following code output?
+	string greeting = "Hello";
+	cout << greeting[2];*/
+	
+		string greeting = "Hello";
+		cout << greeting[1];
+	
+}
