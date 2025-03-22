4a82802b532152cefe457aa8fa57030d58698a7c
Yash Raj Times Tech <yashai4086@gmail.com>
2025-03-22 14:31:01 +0500

    Delete 14.cpp

--- 14.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
- int main (){
- 	
-	/*Write a program that prints the length of the string "C++ Program*/
-	
-	string txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-	cout << "The length of the txt string is: " << txt.length();
-}
