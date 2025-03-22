fcc4973eca5bf39b6f58e3d4b5f389e31c287b68
Yash Raj Times Tech <yashai4086@gmail.com>
2025-03-22 14:33:54 +0500

    Delete 5.cpp

--- 5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
- int main (){
-
-	/*Write a program that concatenates the strings "Good" and "Morning" and prints the result.*/
-	string happy="good ",good="Morning";
-	cout<<""<<happy+good<<endl;
-	
-	}
